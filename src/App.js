3e00dc0431c98b852cbba4c9d02a88d56a8ac58a
daisyjreyes <56975680+daisyjreyes@users.noreply.github.com>
2022-06-27 00:06:16 -0400

    Upload
    First attempt was unsuccessful due to "files too large" "upload 100 at a time"

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+
+function App() {
+  return (
+    // BEM class naming convention 
+    <div className="App">
+      <h1>Lets build the MERN TINDER clone guys!!!</h1>
+    
+      {/* Header */}
+      {/* TinderCards */}
+      {/* SwipeButtons */}
+
+    </div>
+  );
+}
+
+export default App;
